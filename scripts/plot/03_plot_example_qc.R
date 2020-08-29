fa224eddb34b10e02cd1039cbffdd8d3c4ac1c52
rmsandra <20111085@lamolina.edu.pe>
2020-08-29 18:45:03 -0500

    plot example of qc

---
+++ scripts/plot/03_plot_example_qc.R
@@ -0,0 +1,17 @@
+load("./data/output/example_qc.RData")
+
+basins <- raster::shapefile("./data/shapes/CHIRILU.shp")
+
+area_study <- list("sp.polygons", basins,  col="grey10", fill="grey90")
+
+example <- example_obj
+example$QC=example_obj_qc$OBS
+
+#2018-02-15 16:00:00"
+png("./data/output/plots/example_qc.png",width=900,height=700, res = 150)
+       spplot(example, col.regions=rainbow(5),c("OBS", "QC"),
+       key.space = "right", 
+       sp.layout=list(area_study), 
+       scales = list(draw = TRUE),
+       xlim=c(-77.4,-76.0),ylim=c(-12.32,-11.0))
+dev.off()
