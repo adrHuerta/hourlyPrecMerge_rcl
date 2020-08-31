2876f3c6c3cb61ff139108ae00e35cf7d556ac44
rmsandra <20111085@lamolina.edu.pe>
2020-08-30 19:30:40 -0500

    plot example qc

--- test/plot_example_qc.R
+++ test/plot_example_qc.R
@@ -1,17 +1,17 @@
-load("./data/output/example_qc.RData")
 
+data_obs <- readRDS("./data/output/example_qc.rds")
 basins <- raster::shapefile("./data/shapes/CHIRILU.shp")
 
 area_study <- list("sp.polygons", basins,  col="grey10", fill="grey90")
 
-example <- example_obj
-example$QC=example_obj_qc$OBS
+example <- data_obs$no_qc
+example$QC=data_obs$qc$OBS
 
 #2018-02-15 16:00:00"
-png("./data/output/plots/plot_example_qc.png",width=900,height=700, res = 150)
-sp::spplot(example, col.regions=rainbow(5),c("OBS", "QC"),
-       key.space = "right", 
+png("./data/output/plots/plot_example_qc.png",width=900,height=800, res = 150)
+       sp::spplot(example, col.regions=rainbow(5),c("OBS", "QC"),
        sp.layout=list(area_study), 
        scales = list(draw = TRUE),
        xlim=c(-77.4,-76.0),ylim=c(-12.32,-11.0))
 dev.off()
+
