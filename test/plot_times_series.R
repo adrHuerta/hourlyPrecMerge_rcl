62ebf9cf8995dfefeae97f03f8b4485d6b75dd1d
rmsandra <20111085@lamolina.edu.pe>
2020-08-31 12:00:23 -0500

    plot ts obs qc

--- test/plot_times_series.R
+++ test/plot_times_series.R
@@ -1,19 +1,19 @@
 require("xts")
 require("lattice")
 
-obs_data <- readRDS("./data/raw/obs/obs_data.rds")
+obs_data <- readRDS("./data/processed/obs/obs_data_qc.rds")
 
 
-png("./data/output/plots/plot_ts_1.png",width=900,height=700, res = 150)
+png("./data/output/plots/plot_ts_1.png",width=900,height=1000, res = 150)
 xyplot(obs_data$value[,1:23],layout = c(4,6),
-       par.strip.text = list(cex=0.5, lines=0.9),
+       par.strip.text = list(cex=0.7, lines=1.3),
        par.settings = list(strip.background=list(col="gray",lwd=5)),
        xlab="Tiempo",ylab="Precipitación (mm/h)")  
 dev.off()
 
-png("./data/output/plots/plot_ts_2.png",width=900,height=700, res = 150)
+png("./data/output/plots/plot_ts_2.png",width=900,height=1000, res = 150)
 xyplot(obs_data$value[,23:46],layout = c(4,6),
-       par.strip.text = list(cex=0.5, lines=0.9),
+       par.strip.text = list(cex=0.7, lines=1.3),
        par.settings = list(strip.background=list(col="gray",lwd=5)),
        xlab="Tiempo",ylab="Precipitación (mm/h)")  
 dev.off()
