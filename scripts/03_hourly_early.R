44e0a25585ee353a958023d9040fdc9b178f8e30
rmsandra <20111085@lamolina.edu.pe>
2020-10-05 01:03:25 -0500

    extract sat data

--- scripts/03_hourly_early.R
+++ scripts/03_hourly_early.R
@@ -1,12 +1,13 @@
+rm(list = ls())
 "%>%" = magrittr::`%>%`
 source("./src/Check_data.R")
 
 # checking imerg files
-check_imerg_file(location = "./data/raw/early/early_cut",
-                 data_ini = "2014-01-01 05:00",
-                 data_end = "2020-01-01 04:30")
+# check_imerg_file(location = "./data/raw/early/early_cut",
+#                  data_ini = "2014-01-01 05:00",
+#                  data_end = "2020-01-01 04:30")
 
-# obs data
+ # obs data
 obs_data <- readRDS("./data/processed/obs/obs_data_qc_v3.rds")
 
 # list_raster <- list.files("./data/raw/early/early_cut", 
@@ -29,13 +30,24 @@ obs_data <- readRDS("./data/processed/obs/obs_data_qc_v3.rds")
 early_hr <- raster::brick("./data/processed/early/early_chirilu.nc")
 raster::projection(early_hr) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 
-#early time series
+#early time series complete 
 sat_data <- t(raster::extract(early_hr,obs_data$xyz))
 colnames(sat_data) <- colnames(obs_data$value)
-Time  <- zoo::index(obs_data$value)
+Time  <- seq(as.POSIXct("2014-01-01 01:00"), as.POSIXct("2020-01-01 00:00"), by='hour')
 sat_data <- xts::xts(sat_data, order.by = Time)
 
+# extract period 11-03 2014-2019
+ts1 <- sat_data["2014-11/2015-03"]
+ts2 <- sat_data["2015-11/2016-03"]
+ts3 <- sat_data["2016-11/2017-03"]
+ts4 <- sat_data["2017-11/2018-03"]
+ts5 <- sat_data["2018-11/2019-03"]
+ 
+sat_data <- rbind(ts1,ts2,ts3,ts4,ts5)
+ 
 #save
 saveRDS(object=list(value = sat_data, 
                     early_hr = early_hr), 
         file = "./data/processed/sat/sat_data.rds")
+
+
