bd8ed70f8c35046f4071ecd2d62070d2b5d70139
rmsandra <20111085@lamolina.edu.pe>
2020-10-20 23:59:19 -0500

    plot early

--- scripts/plot/02_plot_early.R
+++ scripts/plot/02_plot_early.R
@@ -1,4 +1,5 @@
-
+#plot early an event
+require(raster)
 
 # foldersat="./data/raw/early/2017/02"
 # list_rasters <- list.files() %>%
@@ -13,34 +14,39 @@
 #    })
 #  })
 
-border <- raster::shapefile("./data/shapes/DEPARTAMENTOS.shp")
-basins <- raster::shapefile("./data/shapes/CHIRILU.shp")
-rt1 <- raster::raster("./data/raw/early/2017/03/3B-HHR-E.MS.MRG.3IMERG.20170316-S213000-E215959.1290.V06B.tif")
-rt2 <- raster::raster("./data/raw/early/2017/03/3B-HHR-E.MS.MRG.3IMERG.20170316-S220000-E222959.1320.V06B.tif")
+border <- shapefile("./data/shapes/DEPARTAMENTOS.shp")
+basins <- shapefile("./data/shapes/CHIRILU.shp")
+rt1 <- raster("./data/raw/early/2017/03/3B-HHR-E.MS.MRG.3IMERG.20170316-S213000-E215959.1290.V06B.tif")
+rt2 <- raster("./data/raw/early/2017/03/3B-HHR-E.MS.MRG.3IMERG.20170316-S220000-E222959.1320.V06B.tif")
 r <- rt1+rt2
 
-r_peru <- crop(r, extent(-85,-65.0, -20,1))
-r_chirilu <- crop(r, extent(-77.4,-76.0, -12.32,-11.0))
+r_peru <- crop(r, extent(-85,-65.0, -18.7,0.3))
+r_chirilu <- crop(r, extent(-77.4,-76.0, -12.4,-11.0))
 
 # Set color palette
 zeroCol <-"#ffffff" # #white 
 color <-rainbow(100)
-myTheme <- rasterVis::rasterTheme(region = c(zeroCol,color))
+myTheme <- c(zeroCol,color)
 
 # Plot
 
-png("./data/output/plots/plot_early.png",width=900,height=700, res = 150)
-rasterVis::levelplot(r_peru, par.settings = myTheme, margin = FALSE, 
-          main = expression("Precipitación" ~ (mm ~ hr^{-1})),
-          xlab="",ylab="")+
-          layer(sp.polygons(border, lwd=0.8))
+png("./data/output/plots/plot_early.png",width=900,height=650, res = 150)
+
+sp1 <-sp::spplot(r_peru, col.regions=myTheme,scales = list(draw = FALSE), 
+                 colorkey = list(space = "bottom"),
+                 main=list(label="(a)",cex=0.9))+
+  latticeExtra::layer(sp.polygons(border, lwd=0.8))
+
+
+sp2 <-sp::spplot(r_chirilu, col.regions=myTheme,scales = list(draw = FALSE),
+                 colorkey = list(space = "bottom"),
+                 main=list(label="(b)",cex=0.9))+
+  latticeExtra::layer(sp.polygons(border, lwd=0.8))+
+  latticeExtra::layer(sp.polygons(basins, lwd=0.8))
+
+gridExtra::grid.arrange(sp1,sp2,ncol=2)
 
 dev.off()
 
-png("./data/output/plots/plot_early_chirilu.png",width=900,height=700, res = 150)
-rasterVis::levelplot(r_chirilu, par.settings = myTheme, margin = FALSE, 
-          main = expression("Precipitación" ~ (mm ~ hr^{-1})),
-          xlab="",ylab="")+
-  layer(sp.polygons(border, lwd=0.8))+
-  layer(sp.polygons(basins, lwd=0.8))
-dev.off()
+
+
