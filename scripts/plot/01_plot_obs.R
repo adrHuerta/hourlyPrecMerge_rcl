1607a08a9a535bb58bff361052455e5bfcb4ff79
rmsandra <20111085@lamolina.edu.pe>
2020-10-20 23:26:43 -0500

    plot obs

--- scripts/plot/01_plot_obs.R
+++ scripts/plot/01_plot_obs.R
@@ -13,7 +13,7 @@ xat <- round(seq(extent(r)@xmin,
                  extent(r)@xmax, length.out = 3),2)
 
 sl1 <- list('sp.pointLabel', obs_data$xyz, label=obs_data$xyz$CODE,
-            cex=0.5, col='black')
+            cex=0.4, col='black')
 
 
 png("./data/output/plots/plot_obs.png",width=900,height=700, res = 150) 
@@ -21,11 +21,11 @@ png("./data/output/plots/plot_obs.png",width=900,height=700, res = 150)
 spplot(r,col.regions = viridis_pal(option="D")(255),
       sp.layout=list(list(provs, col="white",fill="gray87",first=TRUE),list(border),sl1), # add it to drive
        scales = list(x = list(at = xat), y = list(at = yat)),
-       xlim=c(-77.4,-76.0),ylim=c(-12.32,-11.1),
+       xlim=c(-77.4,-76.0),ylim=c(-12.32,-11.0),
        par.settings = 
          list(layout.widths = 
          list(right.padding = 7)))+ # to create space on the right and used to legend label 
-       latticeExtra::layer(sp.points(obs_data$xyz,pch =19,lwd=8,col="red2"))+ 
+       latticeExtra::layer(sp.points(obs_data$xyz,pch =19,lwd=3,col="red2"))+ 
        latticeExtra::layer(sp.polygons(basins)) 
        grid.text("Altitud (msnm)", x=unit(0.93, "npc"), y=unit(0.50, "npc"),gp=gpar(fontsize=12), rot=-90) # ok
 
