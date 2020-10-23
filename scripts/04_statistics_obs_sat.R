e19fbd2a755646ad2c8259aef78aa57439027994
rmsandra <20111085@lamolina.edu.pe>
2020-10-23 16:51:25 -0500

    statistics obs sat rainy period

--- scripts/04_statistics_obs_sat.R
+++ scripts/04_statistics_obs_sat.R
@@ -5,7 +5,7 @@ rm(list = ls())
 source('./src/calculate_metrics.R')
 
 #read data
-data_sat <-readRDS("./data/processed/sat/sat_data.rds")
+data_sat <-readRDS("./data/processed/sat/sat_data2.rds")
 data_obs <-readRDS("./data/processed/obs/obs_data_qc_v3.rds")
 
 # type of validation
