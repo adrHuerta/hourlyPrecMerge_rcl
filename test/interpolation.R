577dfaeffcad7b945724c477e6181056a9750a6b
rmsandra <20111085@lamolina.edu.pe>
2020-10-26 00:47:30 -0500

    interpolation test

--- test/interpolation.R
+++ test/interpolation.R
@@ -1,16 +1,8 @@
-rm(list = ls())
 "%>%" = magrittr::`%>%`
 
 source("./test/RIDW_test.R")
 
-#DATA IMERG EARLY 
-sat_data <- readRDS("./data/processed/sat/sat_data2.rds")
-early_hr <- sat_data$early_hr
-#DATA OBS 
-obs_data <- readRDS("./data/processed/obs/obs_data_qc_v3.rds")
-
-
-RIDW_events <- function(events, obs_data, early_hr ) {
+interp_methods <- function(events, obs_data, early_hr , DEM) {
   
                  #select obs in event
                  obs <- obs_data$value[events]
@@ -33,7 +25,7 @@ RIDW_events <- function(events, obs_data, early_hr ) {
                  obs_data$xyz$pp_obs =value
 
                  #average in a single grid
-                 gauge <- Dorado::mean_doble_Station(gauge = obs_data$xyz[9],
+                 gauge <- Dorado::mean_doble_Station(gauge = obs_data$xyz[6],
                                                     cov = early_hr[[i]])
                  gauge@data[gauge@data == "NaN"] <-NA 
 
@@ -42,7 +34,7 @@ RIDW_events <- function(events, obs_data, early_hr ) {
                  sat <- early_hr[[i]]
                  regressionIDW_mode1[[i]] <- RIDW(gauge= gauge,
                                                  cov = sat,
-                                                 cov2=0,
+                                                 cov2= 0,
                                                  formula = pp_obs ~ pp_sat,
                                                  num_cov = "none",
                                                  idpR = seq(0.8, 3.5, 0.1),
@@ -59,8 +51,8 @@ RIDW_events <- function(events, obs_data, early_hr ) {
                  sat2 <- early_hr[[i-2]]
                  regressionIDW_mode3[[i]] <- RIDW(gauge= gauge,
                                                 cov = sat,
-                                                cov2 = list(sat1,sat2),
-                                                formula = pp_obs ~ pp_sat + sat_1 + sat_2,
+                                                cov2 = list(sat1,sat2, DEM),
+                                                formula = pp_obs ~ pp_sat + sat_1 + sat_2 + elev,
                                                 num_cov = "two_hours_before", 
                                                 idpR = seq(0.8, 3.5, 0.1),
                                                 norm = c("sqrt"))
@@ -69,26 +61,12 @@ RIDW_events <- function(events, obs_data, early_hr ) {
                  }
                  #return
                  ridw_mode1 <- raster::stack(regressionIDW_mode1[3:nrow(obs)])
-                 ridw_mode2 <- raster::stack(regressionIDW_mode1[3:nrow(obs)])
-                 ridw_mode3 <- raster::stack(regressionIDW_mode1[3:nrow(obs)])
+                 ridw_mode2 <- raster::stack(regressionIDW_mode2[3:nrow(obs)])
+                 ridw_mode3 <- raster::stack(regressionIDW_mode3[3:nrow(obs)])
                  return(list(RIDW_mode1=ridw_mode1,
                              RIDW_mode2=ridw_mode2,
                              RIDW_mode3=ridw_mode3))
               }
 
 
-event1 <- RIDW_events(events="2015-02-09 07:00/2015-02-10 07:00",
-                      obs_data=obs_data, 
-                      early_hr=early_hr)
 
-
-
-
-# events <- c("2015-02-09 10:00/2015-02-10 07:00",
-#             "2015-03-20 11:00/2015-03-22 03:00",
-#             "2016-02-25 11:00/2016-02-25 23:00",
-#             "2016-02-28 13:00/2016-02-29 10:00",
-#             "2017-03-15 13:00/2017-03-16 03:00",
-#             "2017-03-18 02:00/2017-03-20 03:00",
-#             "2018-01-25 08:00/2018-01-26 02:00",
-#             "2019-02-21 12:00/2019-02-23 03:00")
