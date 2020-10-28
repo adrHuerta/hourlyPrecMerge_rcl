e361281fef01cc813dd494b6de78a0ea8b24d5b2
adrian <adrhuerta@gmail.com>
2020-10-28 00:57:33 -0500

    merging CV

--- scripts/07_mergingCV.R
+++ scripts/07_mergingCV.R
@@ -23,39 +23,39 @@ Events_dates <- list(E1_dates, E2_dates, E3_dates,
                      E7_dates, E8_dates)
 
 # model00: SAT
-lapply(Events_dates, function(event){
-  
-  event_obs_data <- obs$value[ paste(event, collapse = "/") ]
-  event_obs_data <- event_obs_data[, colSums(is.na(event_obs_data)) != nrow(event_obs_data)]
-  XYZ_event <- obs$xyz[match(colnames(event_obs_data), obs$xyz$CODE), ]
-  
-  sapply(1:dim(XYZ_event)[1], function(station_step){
-    
-    XYZ_station_to_validate <- XYZ_event[station_step,]
-    XYZ_station_step <- XYZ_event[-station_step,]
-    event_obs_data_station_step <- event_obs_data[, -station_step]
-    
-    lapply(as.character(time(event_obs_data_station_step)), function(time_step){
-      
-      XYZ_station_step@data["obs"] <- event_obs_data_station_step[time_step,] %>% as.numeric()
-      gauge_points_time <- XYZ_station_step %>% .[complete.cases(.@data),]
-      gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
-      
-      model00 <- gridded_cov_1_time
-      model00
-      
-    }) -> model_res
-    
-    model_res <- raster::brick(model_res)
-    raster::extract(model_res, XYZ_station_to_validate)
-    
-  }) -> CV_data
-  
-  rownames(CV_data) <- as.character(time(event_obs_data))
-  colnames(CV_data) <- XYZ_event@data$CODE
-  CV_data
-  
-}) -> model00_CV_res 
+# lapply(Events_dates, function(event){
+#   
+#   event_obs_data <- obs$value[ paste(event, collapse = "/") ]
+#   event_obs_data <- event_obs_data[, colSums(is.na(event_obs_data)) != nrow(event_obs_data)]
+#   XYZ_event <- obs$xyz[match(colnames(event_obs_data), obs$xyz$CODE), ]
+#   
+#   sapply(1:dim(XYZ_event)[1], function(station_step){
+#     
+#     XYZ_station_to_validate <- XYZ_event[station_step,]
+#     XYZ_station_step <- XYZ_event[-station_step,]
+#     event_obs_data_station_step <- event_obs_data[, -station_step]
+#     
+#     lapply(as.character(time(event_obs_data_station_step)), function(time_step){
+#       
+#       XYZ_station_step@data["obs"] <- event_obs_data_station_step[time_step,] %>% as.numeric()
+#       gauge_points_time <- XYZ_station_step %>% .[complete.cases(.@data),]
+#       gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
+#       
+#       model00 <- gridded_cov_1_time
+#       model00
+#       
+#     }) -> model_res
+#     
+#     model_res <- raster::brick(model_res)
+#     raster::extract(model_res, XYZ_station_to_validate)
+#     
+#   }) -> CV_data
+#   
+#   rownames(CV_data) <- as.character(time(event_obs_data))
+#   colnames(CV_data) <- XYZ_event@data$CODE
+#   CV_data
+#   
+# }) -> model00_CV_res 
 
 # model00: SAT (parallel)
 lapply(Events_dates, function(event){
@@ -94,24 +94,369 @@ lapply(Events_dates, function(event){
 }) -> model00_CV_res
 
 # model01: IDW
+lapply(Events_dates, function(event){
+  
+  event_obs_data <- obs$value[ paste(event, collapse = "/") ]
+  event_obs_data <- event_obs_data[, colSums(is.na(event_obs_data)) != nrow(event_obs_data)]
+  XYZ_event <- obs$xyz[match(colnames(event_obs_data), obs$xyz$CODE), ]
+  
+  sapply(1:dim(XYZ_event)[1], function(station_step){
+    
+    XYZ_station_to_validate <- XYZ_event[station_step,]
+    XYZ_station_step <- XYZ_event[-station_step,]
+    event_obs_data_station_step <- event_obs_data[, -station_step]
+    
+    parallel::mclapply(as.character(time(event_obs_data_station_step)), function(time_step){
+      
+      XYZ_station_step@data["obs"] <- event_obs_data_station_step[time_step,] %>% as.numeric()
+      gauge_points_time <- XYZ_station_step %>% .[complete.cases(.@data),]
+      gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
+      
+      model00 <- IDW(gauge_points = gauge_points_time, gridded_cov = gridded_cov_1_time)
+      model00
+      
+    }, 
+    mc.cores = 5) -> model_res
+    
+    model_res <- raster::brick(model_res)
+    raster::extract(model_res, XYZ_station_to_validate)
+    
+  }) -> CV_data
+  
+  rownames(CV_data) <- as.character(time(event_obs_data))
+  colnames(CV_data) <- XYZ_event@data$CODE
+  CV_data
+  
+}) -> model01_CV_res
 
 # model02: OK
+lapply(Events_dates, function(event){
+  
+  event_obs_data <- obs$value[ paste(event, collapse = "/") ]
+  event_obs_data <- event_obs_data[, colSums(is.na(event_obs_data)) != nrow(event_obs_data)]
+  XYZ_event <- obs$xyz[match(colnames(event_obs_data), obs$xyz$CODE), ]
+  
+  sapply(1:dim(XYZ_event)[1], function(station_step){
+    
+    XYZ_station_to_validate <- XYZ_event[station_step,]
+    XYZ_station_step <- XYZ_event[-station_step,]
+    event_obs_data_station_step <- event_obs_data[, -station_step]
+    
+    parallel::mclapply(as.character(time(event_obs_data_station_step)), function(time_step){
+      
+      XYZ_station_step@data["obs"] <- event_obs_data_station_step[time_step,] %>% as.numeric()
+      gauge_points_time <- XYZ_station_step %>% .[complete.cases(.@data),]
+      gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
+      
+      model00 <- OK(gauge_points = gauge_points_time, gridded_cov = gridded_cov_1_time)
+      model00
+      
+    }, 
+    mc.cores = 5) -> model_res
+    
+    model_res <- raster::brick(model_res)
+    raster::extract(model_res, XYZ_station_to_validate)
+    
+  }) -> CV_data
+  
+  rownames(CV_data) <- as.character(time(event_obs_data))
+  colnames(CV_data) <- XYZ_event@data$CODE
+  CV_data
+  
+}) -> model02_CV_res
 
 # model03: GRA
+lapply(Events_dates, function(event){
+  
+  event_obs_data <- obs$value[ paste(event, collapse = "/") ]
+  event_obs_data <- event_obs_data[, colSums(is.na(event_obs_data)) != nrow(event_obs_data)]
+  XYZ_event <- obs$xyz[match(colnames(event_obs_data), obs$xyz$CODE), ]
+  
+  sapply(1:dim(XYZ_event)[1], function(station_step){
+    
+    XYZ_station_to_validate <- XYZ_event[station_step,]
+    XYZ_station_step <- XYZ_event[-station_step,]
+    event_obs_data_station_step <- event_obs_data[, -station_step]
+    
+    parallel::mclapply(as.character(time(event_obs_data_station_step)), function(time_step){
+      
+      XYZ_station_step@data["obs"] <- event_obs_data_station_step[time_step,] %>% as.numeric()
+      gauge_points_time <- XYZ_station_step %>% .[complete.cases(.@data),]
+      gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
+      
+      model00 <- GRA(gauge_points = gauge_points_time, gridded_cov = gridded_cov_1_time)
+      model00
+      
+    }, 
+    mc.cores = 5) -> model_res
+    
+    model_res <- raster::brick(model_res)
+    raster::extract(model_res, XYZ_station_to_validate)
+    
+  }) -> CV_data
+  
+  rownames(CV_data) <- as.character(time(event_obs_data))
+  colnames(CV_data) <- XYZ_event@data$CODE
+  CV_data
+  
+}) -> model03_CV_res
 
 # model04: GDA
+lapply(Events_dates, function(event){
+  
+  event_obs_data <- obs$value[ paste(event, collapse = "/") ]
+  event_obs_data <- event_obs_data[, colSums(is.na(event_obs_data)) != nrow(event_obs_data)]
+  XYZ_event <- obs$xyz[match(colnames(event_obs_data), obs$xyz$CODE), ]
+  
+  sapply(1:dim(XYZ_event)[1], function(station_step){
+    
+    XYZ_station_to_validate <- XYZ_event[station_step,]
+    XYZ_station_step <- XYZ_event[-station_step,]
+    event_obs_data_station_step <- event_obs_data[, -station_step]
+    
+    parallel::mclapply(as.character(time(event_obs_data_station_step)), function(time_step){
+      
+      XYZ_station_step@data["obs"] <- event_obs_data_station_step[time_step,] %>% as.numeric()
+      gauge_points_time <- XYZ_station_step %>% .[complete.cases(.@data),]
+      gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
+      
+      model00 <- GDA(gauge_points = gauge_points_time, gridded_cov = gridded_cov_1_time)
+      model00
+      
+    }, 
+    mc.cores = 5) -> model_res
+    
+    model_res <- raster::brick(model_res)
+    raster::extract(model_res, XYZ_station_to_validate)
+    
+  }) -> CV_data
+  
+  rownames(CV_data) <- as.character(time(event_obs_data))
+  colnames(CV_data) <- XYZ_event@data$CODE
+  CV_data
+  
+}) -> model04_CV_res
 
 # model05: RIDW
+lapply(Events_dates, function(event){
+  
+  event_obs_data <- obs$value[ paste(event, collapse = "/") ]
+  event_obs_data <- event_obs_data[, colSums(is.na(event_obs_data)) != nrow(event_obs_data)]
+  XYZ_event <- obs$xyz[match(colnames(event_obs_data), obs$xyz$CODE), ]
+  
+  sapply(1:dim(XYZ_event)[1], function(station_step){
+    
+    XYZ_station_to_validate <- XYZ_event[station_step,]
+    XYZ_station_step <- XYZ_event[-station_step,]
+    event_obs_data_station_step <- event_obs_data[, -station_step]
+    
+    parallel::mclapply(as.character(time(event_obs_data_station_step)), function(time_step){
+      
+      XYZ_station_step@data["obs"] <- event_obs_data_station_step[time_step,] %>% as.numeric()
+      gauge_points_time <- XYZ_station_step %>% .[complete.cases(.@data),]
+      gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
+      
+      model00 <- RIDW(gauge_points = gauge_points_time, gridded_cov = gridded_cov_1_time)
+      model00
+      
+    }, 
+    mc.cores = 5) -> model_res
+    
+    model_res <- raster::brick(model_res)
+    raster::extract(model_res, XYZ_station_to_validate)
+    
+  }) -> CV_data
+  
+  rownames(CV_data) <- as.character(time(event_obs_data))
+  colnames(CV_data) <- XYZ_event@data$CODE
+  CV_data
+  
+}) -> model05_CV_res
 
 # model06: RIDW2
+lapply(Events_dates, function(event){
+  
+  event_obs_data <- obs$value[ paste(event, collapse = "/") ]
+  event_obs_data <- event_obs_data[, colSums(is.na(event_obs_data)) != nrow(event_obs_data)]
+  XYZ_event <- obs$xyz[match(colnames(event_obs_data), obs$xyz$CODE), ]
+  
+  sapply(1:dim(XYZ_event)[1], function(station_step){
+    
+    XYZ_station_to_validate <- XYZ_event[station_step,]
+    XYZ_station_step <- XYZ_event[-station_step,]
+    event_obs_data_station_step <- event_obs_data[, -station_step]
+    
+    parallel::mclapply(as.character(time(event_obs_data_station_step)), function(time_step){
+      
+      XYZ_station_step@data["obs"] <- event_obs_data_station_step[time_step,] %>% as.numeric()
+      gauge_points_time <- XYZ_station_step %>% .[complete.cases(.@data),]
+      #gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
+      gridded_cov_2_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 1))
+      
+      model00 <- RIDW(gauge_points = gauge_points_time, gridded_cov = gridded_cov_2_time)
+      model00
+      
+    }, 
+    mc.cores = 5) -> model_res
+    
+    model_res <- raster::brick(model_res)
+    raster::extract(model_res, XYZ_station_to_validate)
+    
+  }) -> CV_data
+  
+  rownames(CV_data) <- as.character(time(event_obs_data))
+  colnames(CV_data) <- XYZ_event@data$CODE
+  CV_data
+  
+}) -> model06_CV_res
 
 # model07: CM_IDW
+lapply(Events_dates, function(event){
+  
+  event_obs_data <- obs$value[ paste(event, collapse = "/") ]
+  event_obs_data <- event_obs_data[, colSums(is.na(event_obs_data)) != nrow(event_obs_data)]
+  XYZ_event <- obs$xyz[match(colnames(event_obs_data), obs$xyz$CODE), ]
+  
+  sapply(1:dim(XYZ_event)[1], function(station_step){
+    
+    XYZ_station_to_validate <- XYZ_event[station_step,]
+    XYZ_station_step <- XYZ_event[-station_step,]
+    event_obs_data_station_step <- event_obs_data[, -station_step]
+    
+    parallel::mclapply(as.character(time(event_obs_data_station_step)), function(time_step){
+      
+      XYZ_station_step@data["obs"] <- event_obs_data_station_step[time_step,] %>% as.numeric()
+      gauge_points_time <- XYZ_station_step %>% .[complete.cases(.@data),]
+      gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
+      #gridded_cov_2_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 1))
+      
+      model00 <- CM_IDW(gauge_points = gauge_points_time, gridded_cov = gridded_cov_1_time)
+      model00
+      
+    }, 
+    mc.cores = 5) -> model_res
+    
+    model_res <- raster::brick(model_res)
+    raster::extract(model_res, XYZ_station_to_validate)
+    
+  }) -> CV_data
+  
+  rownames(CV_data) <- as.character(time(event_obs_data))
+  colnames(CV_data) <- XYZ_event@data$CODE
+  CV_data
+  
+}) -> model07_CV_res
 
 # model08: RK
+lapply(Events_dates, function(event){
+  
+  event_obs_data <- obs$value[ paste(event, collapse = "/") ]
+  event_obs_data <- event_obs_data[, colSums(is.na(event_obs_data)) != nrow(event_obs_data)]
+  XYZ_event <- obs$xyz[match(colnames(event_obs_data), obs$xyz$CODE), ]
+  
+  sapply(1:dim(XYZ_event)[1], function(station_step){
+    
+    XYZ_station_to_validate <- XYZ_event[station_step,]
+    XYZ_station_step <- XYZ_event[-station_step,]
+    event_obs_data_station_step <- event_obs_data[, -station_step]
+    
+    parallel::mclapply(as.character(time(event_obs_data_station_step)), function(time_step){
+      
+      XYZ_station_step@data["obs"] <- event_obs_data_station_step[time_step,] %>% as.numeric()
+      gauge_points_time <- XYZ_station_step %>% .[complete.cases(.@data),]
+      gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
+      #gridded_cov_2_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 1))
+      
+      model00 <- RK(gauge_points = gauge_points_time, gridded_cov = gridded_cov_1_time)
+      model00
+      
+    }, 
+    mc.cores = 5) -> model_res
+    
+    model_res <- raster::brick(model_res)
+    raster::extract(model_res, XYZ_station_to_validate)
+    
+  }) -> CV_data
+  
+  rownames(CV_data) <- as.character(time(event_obs_data))
+  colnames(CV_data) <- XYZ_event@data$CODE
+  CV_data
+  
+}) -> model08_CV_res
 
 # model09: RK2
+lapply(Events_dates, function(event){
+  
+  event_obs_data <- obs$value[ paste(event, collapse = "/") ]
+  event_obs_data <- event_obs_data[, colSums(is.na(event_obs_data)) != nrow(event_obs_data)]
+  XYZ_event <- obs$xyz[match(colnames(event_obs_data), obs$xyz$CODE), ]
+  
+  sapply(1:dim(XYZ_event)[1], function(station_step){
+    
+    XYZ_station_to_validate <- XYZ_event[station_step,]
+    XYZ_station_step <- XYZ_event[-station_step,]
+    event_obs_data_station_step <- event_obs_data[, -station_step]
+    
+    parallel::mclapply(as.character(time(event_obs_data_station_step)), function(time_step){
+      
+      XYZ_station_step@data["obs"] <- event_obs_data_station_step[time_step,] %>% as.numeric()
+      gauge_points_time <- XYZ_station_step %>% .[complete.cases(.@data),]
+      #gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
+      gridded_cov_2_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 1))
+      
+      model00 <- RK(gauge_points = gauge_points_time, gridded_cov = gridded_cov_2_time)
+      model00
+      
+    }, 
+    mc.cores = 5) -> model_res
+    
+    model_res <- raster::brick(model_res)
+    raster::extract(model_res, XYZ_station_to_validate)
+    
+  }) -> CV_data
+  
+  rownames(CV_data) <- as.character(time(event_obs_data))
+  colnames(CV_data) <- XYZ_event@data$CODE
+  CV_data
+  
+}) -> model09_CV_res
 
 # model10: CM_OK
+lapply(Events_dates, function(event){
+  
+  event_obs_data <- obs$value[ paste(event, collapse = "/") ]
+  event_obs_data <- event_obs_data[, colSums(is.na(event_obs_data)) != nrow(event_obs_data)]
+  XYZ_event <- obs$xyz[match(colnames(event_obs_data), obs$xyz$CODE), ]
+  
+  sapply(1:dim(XYZ_event)[1], function(station_step){
+    
+    XYZ_station_to_validate <- XYZ_event[station_step,]
+    XYZ_station_step <- XYZ_event[-station_step,]
+    event_obs_data_station_step <- event_obs_data[, -station_step]
+    
+    parallel::mclapply(as.character(time(event_obs_data_station_step)), function(time_step){
+      
+      XYZ_station_step@data["obs"] <- event_obs_data_station_step[time_step,] %>% as.numeric()
+      gauge_points_time <- XYZ_station_step %>% .[complete.cases(.@data),]
+      gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
+      #gridded_cov_2_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 1))
+      
+      model00 <- CM_OK(gauge_points = gauge_points_time, gridded_cov = gridded_cov_1_time)
+      model00
+      
+    }, 
+    mc.cores = 5) -> model_res
+    
+    model_res <- raster::brick(model_res)
+    raster::extract(model_res, XYZ_station_to_validate)
+    
+  }) -> CV_data
+  
+  rownames(CV_data) <- as.character(time(event_obs_data))
+  colnames(CV_data) <- XYZ_event@data$CODE
+  CV_data
+  
+}) -> model10_CV_res
 
 
 saveRDS(list(model00_CV_res, model01_CV_res, model02_CV_res, model03_CV_res,
