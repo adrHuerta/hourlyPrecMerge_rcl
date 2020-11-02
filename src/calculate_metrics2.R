1e0a63cfef9f98a27b675e535d8d29fc0ad80ee2
rmsandra <20111085@lamolina.edu.pe>
2020-11-02 12:26:54 -0500

    calculate metrics cv

---
+++ src/calculate_metrics2.R
@@ -0,0 +1,50 @@
+calculate_metrics <- function(obs,sat,type_of_validation)
+{
+
+
+  #calculate
+  metrics_calculate <- function(x,y){
+              corr <- hydroGOF::rPearson(y, x)
+              bias <- hydroGOF::pbias(y, x)
+              rmsE <- hydroGOF::rmse(y,x)
+              maE  <- hydroGOF::mae(y,x)
+              MAD  <- median(abs(x-y))
+              # <- hydroGOF::rmse(y,x,na.rm=TRUE)
+              # 
+              # if(bias>100){
+              #   bias=100
+              # } else if (bias<(-100)){
+              #   bias=c(-100)
+              # }else {
+              #   bias=bias
+              # }
+              
+              bias <- c(bias/100)
+              table <- data.frame(corr,bias,MAD,rmsE,maE)
+              }
+  #statistics
+  ls.metrics <- list()
+  
+  #type of validation
+  if(type_of_validation == "temporal"){ 
+    for(i in 1:ncol(obs)){
+      x = zoo::coredata(obs[,i])
+      y = zoo::coredata(sat[,i])
+      ls.metrics[[i]] <- metrics_calculate(x,y)
+    }
+    df_metrics <- dplyr::bind_rows(ls.metrics)
+    return(df_metrics)
+  }
+  else if(type_of_validation == "spatial"){
+      for(i in 1:nrow(obs)){
+      x = t(obs[i,])
+      y = t(sat[i,])
+      ls.metrics[[i]] <- metrics_calculate(x,y)
+      }
+    df_metrics <- dplyr::bind_rows(ls.metrics)
+    return(df_metrics)
+  }
+     
+}
+
+
