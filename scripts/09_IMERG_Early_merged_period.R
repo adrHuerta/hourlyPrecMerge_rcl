7f4caa54998f7c4cc1abc75f714b00b7247c0aa3
rmsandra <20111085@lamolina.edu.pe>
2020-11-03 13:47:29 -0500

    IMERG Early merged

--- scripts/09_IMERG_Early_merged_period.R
+++ scripts/09_IMERG_Early_merged_period.R
@@ -2,8 +2,6 @@
 # Merged of observed and GPM IMERG Early, data median of the methods applied 
 # to improve the satellite estimation
 # 
-# written by 
-
 
 rm(list = ls())
 "%>%" = magrittr::`%>%`
@@ -17,13 +15,23 @@ source("./src/points_inside_pixel.R")
 obs <- readRDS("./data/processed/obs/obs_data_qc_v2.rds")
 sat <-readRDS("./data/processed/sat/sat_data.rds")$early
 
-
+### 1.-Remover estaciones que no adecuadas
 #data obs 2014-2019
 obs$value <- obs$value["2014-01/2019-12"]
 
 # remove ID's Out of limit
 df_obs <- data.frame(obs$value)
 df_obs<- dplyr::select(df_obs,-ID472364DC,-ID110137,-ID47257764)
+
+# remove by climatology montly
+df_obs<- dplyr::select(df_obs,-ID472542FE,-ID47A0A5A6,-ID4721725E)
+
+# remove by climatology hourly
+df_obs <- dplyr::select(df_obs,-ID4726B574)
+
+# remove by QC
+df_obs <- dplyr::select(df_obs ,-ID472A0766,-ID47E33064)
+
 
 #to object xts and spdf
 obs$value <- xts::xts(df_obs,order.by = index(obs$value))
@@ -36,13 +44,42 @@ obs$xyz <- sp::SpatialPointsDataFrame(coords=df_xyz[,3:4],
                                            proj4string=sp::CRS(" +proj=longlat +datum=WGS84 +no_defs 
                                                                +ellps=WGS84 +towgs84=0,0,0") )
 
+### 2.-Una sola estación por pixel
+# getting single points 
+new_xyz <- make_single_point(pts = obs$xyz, rgrid = sat)$xyz
 
-#reproyectar  
-raster::projection(obs$xyz) <- raster::projection(sat)
+# points to be merged
+id_to_merge <- make_single_point(pts = obs$xyz, rgrid = sat)$no_single_points
 
-#merging 2014-2019
+# single ts
+new_value <- obs$value[, -match(unlist(id_to_merge), colnames(obs$value))]
 
-for(i in 2:nrow(obs$value)){
+# merging no-single ts
+id_to_merge <- id_to_merge %>%
+  setNames(id_to_merge %>% lapply(function(x) paste(x, collapse = "_"))) %>%
+  lapply(function(x){
+    res <- obs$value[, x] %>% apply(1, mean, na.rm = TRUE)
+    res[is.nan(res)] <- NA
+    res
+  }) %>%
+  do.call("cbind", .)
+
+id_to_merge <- xts::xts(id_to_merge, time(obs$value))
+
+# single + new single ts
+new_value <- cbind(new_value, id_to_merge)
+
+
+obs <-list(value = round(new_value, 1),
+             xyz = new_xyz)
+
+# date<-as.character(index(obs$value))
+# which(date=="2015-01-01 00:00:00")
+time_ini <-  8760
+
+### 3.-Producto grillado CHIRILU
+
+for(i in time_ini:nrow(obs$value)){
   
   data_obs <- obs$value[i,]
   data_xyz <- obs$xyz
@@ -50,25 +87,22 @@ for(i in 2:nrow(obs$value)){
   
   label_date <-gsub(":", ".",as.character(index(data_obs)))
   
-  # EMA-grilla
-  data_obs<- make_single_point(pts = data_xyz,
-                                rgrid = sat[[i]])$xyz %>%
+ 
+  data_obs<- data_xyz %>%
     .[complete.cases(.@data),]
   
   colnames(data_obs@data)[9] <- "obs"
   
-  
-  
-  
-  if(sum(is.na(obs$value[i,]))==ncol(obs$value)){
 
-    chirilu_gridded = raster::raster(nrow=13, ncol=14)
+  if(sum(is.na(obs$value[i,]))==ncol(obs$value)){
     
-    raster::writeRaster(chirilu_gridded, 
-                        file=paste0("./data/processed/merging/IMERG-Early_merging_",label_date,".tiff"),overwrite=TRUE)
+    chirilu_gridded = NA
     
+  } else if(sum(is.na(obs$value[i,])) < 9 ) {
     
-  } else  {
+    chirilu_gridded = sat[[i]]
+  
+  } else if(sum(is.na(obs$value[i,])) >= 9 ){
     
     # Mezcla
     IDW_res<- IDW(gauge_points = data_obs, gridded_cov = sat[[i]])
@@ -79,27 +113,20 @@ for(i in 2:nrow(obs$value)){
     RK_res <- RK(gauge_points = data_obs, gridded_cov = sat[[i]])
     RK2_res <- RK(gauge_points = data_obs, gridded_cov = sat[[c(i-1):i]])
     CM_OK_res <- CM_OK(gauge_points = data_obs, gridded_cov = sat[[i]])
-  
+    
     chirilu_gridded<- raster::brick(IDW_res, OK_res,
-                                     RIDW_res, RIDW2_res, CM_IDW_res,
-                                     RK_res, RK2_res, CM_OK_res) %>%
+                                    RIDW_res, RIDW2_res, CM_IDW_res,
+                                    RK_res, RK2_res, CM_OK_res) %>%
     raster::calc(fun = function(x) median(x, na.rm = TRUE))
     
-   
+    
     
     raster::writeRaster(chirilu_gridded, 
-                        file=paste0("./data/processed/merging/IMERG-Early_merging_",label_date,".tiff"),overwrite=TRUE)
+                        file=paste0("./data/processed/merging/Chirilu.v2",label_date,".tiff"),overwrite=TRUE)
     
-    }
+  }
   
 }
-
-
-
-  
-  
-  
-
 
 
 
