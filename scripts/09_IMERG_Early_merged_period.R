0ee38440288a0452a5c4d9fdf3ff935fa218716f
rmsandra <20111085@lamolina.edu.pe>
2020-11-03 02:53:48 -0500

    IMERG early merged 2014-2019

--- scripts/09_IMERG_Early_merged_period.R
+++ scripts/09_IMERG_Early_merged_period.R
@@ -12,6 +12,7 @@ library(xts)
 library(raster)
 
 source("./src/interpolation.R")
+source("./src/points_inside_pixel.R")
 
 obs <- readRDS("./data/processed/obs/obs_data_qc_v2.rds")
 sat <-readRDS("./data/processed/sat/sat_data.rds")$early
@@ -19,268 +20,79 @@ sat <-readRDS("./data/processed/sat/sat_data.rds")$early
 
 #data obs 2014-2019
 obs$value <- obs$value["2014-01/2019-12"]
-crs(obs$xyz) = "+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs"
+
+# remove ID's Out of limit
+df_obs <- data.frame(obs$value)
+df_obs<- dplyr::select(df_obs,-ID472364DC,-ID110137,-ID47257764)
+
+#to object xts and spdf
+obs$value <- xts::xts(df_obs,order.by = index(obs$value))
+
+df_xyz <- data.frame(obs$xyz)
+df_xyz<- dplyr::filter(df_xyz, CODE %in% colnames(obs$value))
+
+obs$xyz <- sp::SpatialPointsDataFrame(coords=df_xyz[,3:4],
+                                           data=df_xyz,
+                                           proj4string=sp::CRS(" +proj=longlat +datum=WGS84 +no_defs 
+                                                               +ellps=WGS84 +towgs84=0,0,0") )
 
 
-# Period
-dates <- c("2019-01-01 00:00:00", "2019-01-02 00:00:00")
-dates <- list(dates)
+#reproyectar  
+raster::projection(obs$xyz) <- raster::projection(sat)
 
-############### MERGING METHODS 
-# model00: SAT
-lapply(dates, function(period){
+#merging 2014-2019
+
+for(i in 2:nrow(obs$value)){
   
-    period_obs_data <- obs$value[ paste(period, collapse = "/") ]
-    period_obs_data <- period_obs_data[, colSums(is.na(period_obs_data)) != nrow(period_obs_data)]
-    XYZ_period <- obs$xyz[match(colnames(period_obs_data), obs$xyz$CODE), ]
+  data_obs <- obs$value[i,]
+  data_xyz <- obs$xyz
+  data_xyz$OBS=as.numeric(data_obs)
   
-    lapply(as.character(time(period_obs_data)), function(time_step){
+  # EMA-grilla
+  data_obs<- make_single_point(pts = data_xyz,
+                                rgrid = sat[[i]])$xyz %>%
+    .[complete.cases(.@data),]
+  
+  colnames(data_obs@data)[9] <- "obs"
+  
+  if(sum(is.na(obs$value[i,]))==ncol(obs$value)){
+    chirilu_gridded = NA
+  } else  {
     
-    XYZ_period@data["obs"] <- period_obs_data[time_step,] %>% as.numeric()
-    gauge_points_time <- XYZ_period %>% .[complete.cases(.@data),]
-    gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
- 
-    model00 <- gridded_cov_1_time
-    names(model00) <- time_step
-    model00 <- raster::setZ(model00, time_step)
-    model00
+    # Mezcla
+    IDW_res<- IDW(gauge_points = data_obs, gridded_cov = sat[[i]])
+    OK_res <- OK(gauge_points = data_obs, gridded_cov = sat[[i]])
+    RIDW_res <- RIDW(gauge_points = data_obs, gridded_cov = sat[[i]])
+    RIDW2_res <- RIDW(gauge_points = data_obs, gridded_cov = sat[[c(i-1):i]])
+    CM_IDW_res <- CM_IDW(gauge_points = data_obs, gridded_cov = sat[[i]])
+    RK_res <- RK(gauge_points = data_obs, gridded_cov = sat[[i]])
+    RK2_res <- RK(gauge_points = data_obs, gridded_cov = sat[[c(i-1):i]])
+    CM_OK_res <- CM_OK(gauge_points = data_obs, gridded_cov = sat[[i]])
+  
+    chirilu_gridded<- raster::brick(IDW_res, OK_res,
+                                     RIDW_res, RIDW2_res, CM_IDW_res,
+                                     RK_res, RK2_res, CM_OK_res) %>%
+    raster::calc(fun = function(x) median(x, na.rm = TRUE))
     
-  }) -> model_res
-  
-  raster::brick(model_res)  
-  
-}) -> model00_res 
-
-# model01: IDW
-lapply(dates, function(period){
-  
-    period_obs_data <- obs$value[ paste(period, collapse = "/") ]
-    period_obs_data <- period_obs_data[, colSums(is.na(period_obs_data)) != nrow(period_obs_data)]
-    XYZ_period <- obs$xyz[match(colnames(period_obs_data), obs$xyz$CODE), ]
-  
-    lapply(as.character(time(period_obs_data)), function(time_step){
+    label_date <-gsub(":", ".",as.character(index(data_obs)))
     
-    XYZ_period@data["obs"] <- period_obs_data[time_step,] %>% as.numeric()
-    gauge_points_time <- XYZ_period %>% .[complete.cases(.@data),]
-    gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
-     
-    model00 <- IDW(gauge_points = gauge_points_time, gridded_cov = gridded_cov_1_time)
-    names(model00) <- time_step
-    model00 <- raster::setZ(model00, time_step)
-    model00
+    raster::writeRaster(chirilu_gridded, 
+                        file=paste0("./data/processed/merging/IMERG-Early_merging_",label_date,".tiff"),overwrite=TRUE)
     
-  }) -> model_res
+    }
   
-  raster::brick(model_res)  
-  
-}) -> model01_res 
-
-# model02: OK
-lapply(dates, function(period){
-  
-  period_obs_data <- obs$value[ paste(period, collapse = "/") ]
-  period_obs_data <- period_obs_data[, colSums(is.na(period_obs_data)) != nrow(period_obs_data)]
-  XYZ_period <- obs$xyz[match(colnames(period_obs_data), obs$xyz$CODE), ]
-  
-  lapply(as.character(time(period_obs_data)), function(time_step){
-    
-    XYZ_period@data["obs"] <- period_obs_data[time_step,] %>% as.numeric()
-    gauge_points_time <- XYZ_period %>% .[complete.cases(.@data),]
-    gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
-    
-    model00 <- OK(gauge_points = gauge_points_time, gridded_cov = gridded_cov_1_time)
-    names(model00) <- time_step
-    model00 <- raster::setZ(model00, time_step)
-    model00
-    
-  }) -> model_res
-  
-  raster::brick(model_res)  
-  
-}) -> model02_res 
+}
 
 
-# model03: RIDW
-lapply(dates, function(period){
-  
-    period_obs_data <- obs$value[ paste(period, collapse = "/") ]
-    period_obs_data <- period_obs_data[, colSums(is.na(period_obs_data)) != nrow(period_obs_data)]
-    XYZ_period <- obs$xyz[match(colnames(period_obs_data), obs$xyz$CODE), ]
-  
-    lapply(as.character(time(period_obs_data)), function(time_step){
-    
-    XYZ_period@data["obs"] <- period_obs_data[time_step,] %>% as.numeric()
-    gauge_points_time <- XYZ_period %>% .[complete.cases(.@data),]
-    gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
-      
-    model00 <- RIDW(gauge_points = gauge_points_time, gridded_cov = gridded_cov_1_time)
-    names(model00) <- time_step
-    model00 <- raster::setZ(model00, time_step)
-    model00
-    
-  }) -> model_res
-  
-  raster::brick(model_res)  
-  
-}) -> model03_res 
 
-# model04: RIDW2
-lapply(dates, function(period){
   
-    period_obs_data <- obs$value[ paste(period, collapse = "/") ]
-    period_obs_data <- period_obs_data[, colSums(is.na(period_obs_data)) != nrow(period_obs_data)]
-    XYZ_period <- obs$xyz[match(colnames(period_obs_data), obs$xyz$CODE), ]
   
-    lapply(as.character(time(period_obs_data)), function(time_step){
-    
-    XYZ_period@data["obs"] <- period_obs_data[time_step,] %>% as.numeric()
-    gauge_points_time <- XYZ_period %>% .[complete.cases(.@data),]
-    gridded_cov_2_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 1))
-    
-    model00 <- RIDW(gauge_points = gauge_points_time, gridded_cov = gridded_cov_2_time)
-    names(model00) <- time_step
-    model00 <- raster::setZ(model00, time_step)
-    model00
-    
-  }) -> model_res
   
-  raster::brick(model_res)  
-  
-}) -> model04_res 
-
-# model05: CM_IDW
-lapply(dates, function(period){
-  
-    period_obs_data <- obs$value[ paste(period, collapse = "/") ]
-    period_obs_data <- period_obs_data[, colSums(is.na(period_obs_data)) != nrow(period_obs_data)]
-    XYZ_period <- obs$xyz[match(colnames(period_obs_data), obs$xyz$CODE), ]
-  
-    lapply(as.character(time(period_obs_data)), function(time_step){
-    
-    XYZ_period@data["obs"] <- period_obs_data[time_step,] %>% as.numeric()
-    gauge_points_time <- XYZ_period %>% .[complete.cases(.@data),]
-    gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
-   
-    model00 <- CM_IDW(gauge_points = gauge_points_time, gridded_cov = gridded_cov_1_time)
-    names(model00) <- time_step
-    model00 <- raster::setZ(model00, time_step)
-    model00
-    
-  }) -> model_res
-  
-  raster::brick(model_res)  
-  
-}) -> model05_res 
-
-# model06: RK
-lapply(dates, function(period){
-  
-    period_obs_data <- obs$value[ paste(period, collapse = "/") ]
-    period_obs_data <- period_obs_data[, colSums(is.na(period_obs_data)) != nrow(period_obs_data)]
-    XYZ_period <- obs$xyz[match(colnames(period_obs_data), obs$xyz$CODE), ]
-  
-    lapply(as.character(time(period_obs_data)), function(time_step){
-    
-    XYZ_period@data["obs"] <- period_obs_data[time_step,] %>% as.numeric()
-    gauge_points_time <- XYZ_period %>% .[complete.cases(.@data),]
-    gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
-
-    model00 <- RK(gauge_points = gauge_points_time, gridded_cov = gridded_cov_1_time)
-    names(model00) <- time_step
-    model00 <- raster::setZ(model00, time_step)
-    model00
-    
-  }) -> model_res
-  
-  raster::brick(model_res)  
-  
-}) -> model06_res 
-
-# model07: RK2
-lapply(dates, function(period){
-  
-    period_obs_data <- obs$value[ paste(period, collapse = "/") ]
-    period_obs_data <- period_obs_data[, colSums(is.na(period_obs_data)) != nrow(period_obs_data)]
-    XYZ_period <- obs$xyz[match(colnames(period_obs_data), obs$xyz$CODE), ]
-  
-    lapply(as.character(time(period_obs_data)), function(time_step){
-    
-    XYZ_period@data["obs"] <- period_obs_data[time_step,] %>% as.numeric()
-    gauge_points_time <- XYZ_period %>% .[complete.cases(.@data),]
-    gridded_cov_2_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 1))
-    
-    model00 <- RK(gauge_points = gauge_points_time, gridded_cov = gridded_cov_2_time)
-    names(model00) <- time_step
-    model00 <- raster::setZ(model00, time_step)
-    model00
-    
-  }) -> model_res
-  
-  raster::brick(model_res)  
-  
-}) -> model07_res 
-
-# model08: CM_OK
-lapply(dates, function(period){
-  
-    period_obs_data <- obs$value[ paste(period, collapse = "/") ]
-    period_obs_data <- period_obs_data[, colSums(is.na(period_obs_data)) != nrow(period_obs_data)]
-    XYZ_period <- obs$xyz[match(colnames(period_obs_data), obs$xyz$CODE), ]
-  
-    lapply(as.character(time(period_obs_data)), function(time_step){
-    
-    XYZ_period@data["obs"] <- period_obs_data[time_step,] %>% as.numeric()
-    gauge_points_time <- XYZ_period %>% .[complete.cases(.@data),]
-    gridded_cov_1_time <- subset(sat, which(raster::getZ(sat) == time_step) - seq(0:0, 0))
-
-    model00 <- CM_OK(gauge_points = gauge_points_time, gridded_cov = gridded_cov_1_time)
-    names(model00) <- time_step
-    model00 <- raster::setZ(model00, time_step)
-    model00
-    
-  }) -> model_res
-  
-  raster::brick(model_res)  
-  
-}) -> model08_res
 
 
-merging_grid <-  list(model00_res, model01_res, model02_res, model03_res,
-                      model04_res, model05_res, model06_res, model07_res, model08_res)
 
 
-############### METHODS median 
-names(merging_grid) <- c("SAT", 
-                         "IDW", "OK",
-                         "RIDW", "RIDW2","CM_IDW",
-                         "RK", "RK2", "CM_OK")
-
-merging <- Map(function(x, y) setNames(x, "Period"),
-                      merging, names(merging))
 
 
-median_value <- lapply(merging,
-                       function(x){
-                         x$Period
-                       })
 
-(1:raster::nlayers(median_value$IDW)) %>%
-  lapply(function(x){
-    raster::calc(raster::brick(median_value$IDW[[x]],
-                               median_value$OK[[x]],
-                               median_value$RIDW[[x]],
-                               median_value$RIDW2[[x]],
-                               median_value$CM_IDW[[x]],
-                               median_value$RK[[x]],
-                               median_value$RK2[[x]],
-                               median_value$CM_OK[[x]]),
-                 function(z) median(z, na.rm = TRUE))
-  }) -> median_value
-
-res <- raster::brick(median_value)
-names(res) <- names(merging_events$SAT$Period)
-
-#### SAVE
-saveRDS(IMERG_merged=res, 
-                file="./data/precessed/IMERG_Early_merged_2014-2019.rds")
 
